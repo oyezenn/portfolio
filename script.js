b2d37229dd16b39f8ca97cfbde2f0704f98097da
oyezenn <chandrikalamani0088@gmail.com>
2025-10-13 22:35:09 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.body.style.opacity = 0;
+  setTimeout(() => {
+    document.body.style.transition = "opacity 1.5s";
+    document.body.style.opacity = 1;
+  }, 100);
+});
